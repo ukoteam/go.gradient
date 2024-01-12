f9bc4826b6f2d981bcaec00ac0821af21a782c6f
Selanis <k.polexowa@yandex.ru>
2024-01-12 20:17:13 +0300

    Работа с файлами куки

--- js/favorite.js
+++ js/favorite.js
@@ -1,3 +1,30 @@
+document.addEventListener('DOMContentLoaded', function(e) {
+    var cook = document.cookie
+    console.log(cook)
+})
+
+let allCards = document.querySelectorAll('.flag');
+console.log(allCards);
+
+function ReloadMap() {
+    var dictFav = new Map();
+
+    let date = new Date(Date.now() + 6.307e7);
+    date = date.toUTCString();
+
+    for (var i = 0; i < allCards.length; i++) {
+        dictFav.set("favcard-" + i.toString(), allCards[i].classList[1])
+        
+            document.cookie = `favcard-${i.toString()}=${allCards[i].classList[1]}; max-age=63000000`;
+        
+        
+    }
+
+    console.log(dictFav)
+}
+
+
+
 function FavoritesClick(e) {
     let cardAll = document.getElementById(e).querySelector(".flag");
 
@@ -11,4 +38,8 @@ function FavoritesClick(e) {
             cardAll.classList.add("true");
             break;
     }
+
+    ReloadMap();
 }
+
+
