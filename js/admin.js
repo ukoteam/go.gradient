5fb0c0d04d6e0adbcbe10cdc6e5052a516ba4a9a
Selanis <k.polexowa@yandex.ru>
2024-02-09 16:45:26 +0300

    Добавила предпросмотр карточки

--- js/admin.js
+++ js/admin.js
@@ -1,8 +1,8 @@
 let newButton = document.getElementById('newButton'),
-    newForm = document.getElementById('newForm').querySelector('.service-form__form')
+    newForm = document.getElementById('newForm').querySelector('.service-form')
 
 newButton.addEventListener('click', () => {
-    newForm.classList.toggle('hide');
+    newForm.classList.toggle('shown');
 
     let minus = newButton.querySelector('.button-plus')
     minus.classList.toggle('minus');
@@ -25,3 +25,41 @@ newBigName.addEventListener('input', () => {
 newDescription.addEventListener('input', () => {
     document.getElementById('cardNewBG').innerHTML = newDescription.value;
 });
+
+let form = document.getElementById('form')
+form.addEventListener('keydown', function(event) {
+    if(event.keyCode == 13) {
+        event.preventDefault();
+    }
+});
+
+// Сохранение картинки
+let newLogo = document.getElementById('newLogo'),
+    newLabelLogo = document.getElementById('newLabelLogo')
+
+// function save (selectedFile)  {
+//     let image = document.getElementById('logo');
+//     let f = selectedFile.files[0];
+//     if (f) {
+//         image.style.background = URL.createObjectURL(f);
+//         localStorage.setItem('myImage', image.style.background);
+//     }
+// }
+
+
+
+newLogo.addEventListener('change', () => {
+    const selectedFile = document.getElementById("newLogo").files[0];
+    
+
+    if (selectedFile.type == 'image/png' || selectedFile.type == 'image/jpg' || selectedFile.type == 'image/jpeg') {
+        var i = URL.createObjectURL(selectedFile)
+        console.log(selectedFile);
+        console.log(i);
+        // save(selectedFile) 
+
+        let image = document.getElementById('logo');
+        image.src = i
+    }
+})
+
