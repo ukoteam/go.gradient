bb6ddb366c48cc685517ddef782f5c33677242cd
Selanis <k.polexowa@yandex.ru>
2024-02-19 11:01:46 +0300

    Поэкспериментировала с express

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 const fs = require("fs");
+const express = require("express");
+// const multer = require("multer");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 // const newData = {
 //     id: "test",
@@ -20,29 +24,43 @@ const fs = require("fs");
 
 // console.log(parseData)
 
-document.getElementById("addNewService").addEventListener('click', () => {
-    const newData = {
-        id: "test",
-        name: document.getElementById("newName").value,
-        nameDescription: document.getElementById("newBigName").value,
-        logo: "...", 
-        description: document.getElementById("newDescription").value,
-        background: "../src/common.png",
-        link: document.getElementById("newLink").value
-    };
+const app = express();
+const port = process.env.PORT || 3000;
 
-    console.log('OK')
+app.use(express.static(path.resolve(__dirname, '..')))
 
-    let raw = fs.readFileSync('../json/cards.json');
-    let parseData = JSON.parse(raw);
-    let newObject = JSON.stringify(newData);
-    newObject = JSON.parse(newObject);
-    parseData.cards.push(newObject);
-    parseData = JSON.stringify(parseData);
-    fs.writeFileSync('../json/cards.json', parseData)
+// app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname,'..', 'admin.html'));
+// })
 
-    console.log(parseData)
-
-    console.log('OK')
-
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`)
 })
+
+
+// document.getElementById("addNewService").addEventListener('click', () => {
+//     const newData = {
+//         id: "test",
+//         name: document.getElementById("newName").value,
+//         nameDescription: document.getElementById("newBigName").value,
+//         logo: "...", 
+//         description: document.getElementById("newDescription").value,
+//         background: "../src/common.png",
+//         link: document.getElementById("newLink").value
+//     };
+
+//     console.log('OK')
+
+//     let raw = fs.readFileSync('../json/cards.json');
+//     let parseData = JSON.parse(raw);
+//     let newObject = JSON.stringify(newData);
+//     newObject = JSON.parse(newObject);
+//     parseData.cards.push(newObject);
+//     parseData = JSON.stringify(parseData);
+//     fs.writeFileSync('../json/cards.json', parseData)
+
+//     console.log(parseData)
+
+//     console.log('OK')
+
+// })
